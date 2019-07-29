5c94ccc4facac964cfa3743a0013b16636451e39
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-29 21:29:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,10 @@
 def reverse_each_word(array)
   myArray = array.split(" ")
   returnstring = ""
-  myArray.each_with_index do |n, index|
-      myArray[index] =  myArray[index].reverse()
-  end
+  myArray.collect{|n| n.reverse()}
+  #myArray.each_with_index do |n, index|
+  #    myArray[index] =  myArray[index].reverse()
+#  end
+  
   myArray.join(" ")
 end
