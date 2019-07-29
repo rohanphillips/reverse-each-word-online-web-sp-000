562d069fddcb43e75ad7f17f13f83f8f4cc1c266
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-29 21:32:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
 def reverse_each_word(array)
   myArray = array.split(" ")
   myArray2 = array.split(" ")
-  
+
   myArray2.collect{|n| n.reverse()}
   myArray.each_with_index do |n, index|
       myArray[index] =  myArray[index].reverse()
